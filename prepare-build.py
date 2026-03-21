5ddd7df192a59c36af579306776929e788a78f1c
laund <me@laund.moe>
2026-03-22 00:27:26 +0100

    Update prepare-build.py

--- prepare-build.py
+++ prepare-build.py
@@ -192,7 +192,7 @@ def generate_combined_workflow(workflows, tracy_tag):
         "name": "Combined Tracy Build",
         "on": {"push": {"tags": ["v*"]}},
         "permissions": {"contents": "write"},
-        "env": {"CMAKE_BUILD_PARALLEL_LEVEL": 3},
+        "env": {"CMAKE_BUILD_PARALLEL_LEVEL": 2},
         "jobs": {},
     }
 
